59fcbbc572e5787b0e41902238c562e5570723bb
Sanikhsan12 <lordkazumasan12@gmail.com>
2025-12-17 23:53:04 +0700

    update splash screen

--- lib/view/splash_screen_page.dart
+++ lib/view/splash_screen_page.dart
@@ -4,6 +4,8 @@ import 'login_page.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({super.key});
+
   @override
   _SplashScreenPageState createState() => _SplashScreenPageState();
 }
@@ -12,7 +14,7 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () {
+    Timer(Duration(seconds: 5), () {
       Navigator.of(
         context,
       ).pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
@@ -22,10 +24,15 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       body: Center(
-        child: LoadingAnimationWidget.staggeredDotsWave(
-          color: Colors.blue,
-          size: 100,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.egg_outlined, size: 150, color: Colors.white),
+            SizedBox(height: 32),
+            LoadingAnimationWidget.waveDots(color: Colors.white, size: 50),
+          ],
         ),
       ),
     );
